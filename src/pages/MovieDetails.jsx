49d0a2f96a1361b78f3a21b54b11b32c31e74bb8
DimaKrupenko <dimakrupenko77@gmail.com>
2023-01-24 18:21:38 +0200

    upd

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -28,25 +28,49 @@ const MovieDetails = () => {
   }, [movieId, Id]);
 
   return (
-    <div>
+    <div
+       style={{
+       
+        display: 'flex',
+        gap: '30px',
+       flexWrap: 'wrap'
+      }}
+    >
+      
       <img
         src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
         alt=""
       />
+ 
       <h2>
         {details.original_title}
         <br />
         {details.release_date}
       </h2>
-      <p>User score: {details.popularity}</p>
-      <p>
-        Overview <br />
+      <p
+        style={{
+          width: '1500px'
+        }}
+      >User score: {details.popularity}</p>
+      <h3>Overview</h3>
+      <p
+        style={{
+          width: '1500px'
+        }}
+      >
+        
         {details.overview}
       </p>
       {/* <p>
         Genres <br />
         {details.genres.map(genre => genre.name)}
       </p> */}
+       
+      
+      <h3
+        style={{
+          width: '1500px'
+        }}>Additional information</h3>
       <p>
         <Link to='Cast'>Cast</Link>
       </p>
