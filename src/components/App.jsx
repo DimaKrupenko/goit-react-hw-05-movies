eabe9442f3e6f276b82a25413d9aca4eb6b8c305
DimaKrupenko <dimakrupenko77@gmail.com>
2023-01-25 13:21:31 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,7 @@ export const App = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route index element={<Home />} />
-          <Route baseURL element={<Home />} />
+          <Route baseUrl element={<Home />} />
           <Route path='*' element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path=":Id" element={<MovieDetails />}>
